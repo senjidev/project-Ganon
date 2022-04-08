e18b0d810edaf01ebd998007e5be888e6d1ef51a
senji <100248680+senjidev@users.noreply.github.com>
2022-04-08 06:12:19 -0400

    base window created, and pygame added to venv via requirements file

---
+++ main.py
@@ -0,0 +1,44 @@
+from operator import is_
+from pygame.locals import *
+import pygame as pg, sys
+
+"""GLOABALS"""
+global is_running
+is_running = True
+
+
+#init
+pg.init()
+
+#window config
+pg.display.set_caption("project-ganon")
+WINDOW_SIZE = (800,800)
+global screen
+screen = pg.display.set_mode(WINDOW_SIZE, 0, 32)
+
+#fps clock
+clock = pg.time.Clock()
+
+
+
+"""GAME LOOP"""
+def mainLoop():
+    while is_running:
+        #bg color
+        screen.fill((255, 0, 0))
+
+        #event handler
+        for event in pg.event.get():
+            """EVENTS"""
+            #game closed
+            if event.type == QUIT:
+                print('Game Closed')
+                pg.quit()
+                sys.exit()
+        
+        pg.display.update() #window refresh
+        clock.tick(60) #fps set
+
+
+if __name__ == '__main__':
+    mainLoop()
